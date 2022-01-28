a1e63ce6196d1fab7eacc70a8d0738fd57424cf2
NakayamaKento <kento427@outlook.com>
2022-01-29 03:09:02 +0900

    initial commit

---
+++ Activate_AADJ_ST.ps1
@@ -0,0 +1,86 @@
+Install-Module -Name Az.Storage
+Install-Module -Name AzureAD
+
+#Please Change the Paramater
+$tenantId = "<MyTenantId>"
+$subscriptionId = "<MySubscriptionId>"
+$resourceGroupName = "<MyResourceGroup>"
+$storageAccountName = "<MyStorageAccount>"
+
+
+#Enable Azure AD authentication
+Connect-AzAccount -Tenant $tenantId -SubscriptionId $subscriptionId
+
+$Uri = ('https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Storage/storageAccounts/{2}?api-version=2021-04-01' -f $subscriptionId, $resourceGroupName, $storageAccountName);
+
+$json = @{properties=@{azureFilesIdentityBasedAuthentication=@{directoryServiceOptions="AADKERB"}}};
+$json = $json | ConvertTo-Json -Depth 99
+
+$token = $(Get-AzAccessToken).Token
+$headers = @{ Authorization="Bearer $token" }
+
+try {
+    Invoke-RestMethod -Uri $Uri -ContentType 'application/json' -Method PATCH -Headers $Headers -Body $json;
+} catch {
+    Write-Host $_.Exception.ToString()
+    Write-Error -Message "Caught exception setting Storage Account directoryServiceOptions=AADKERB: $_" -ErrorAction Stop
+}
+
+
+#Generate the kerb1 storage account key for your storage account
+New-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName -KeyName kerb1 -ErrorAction Stop
+
+
+#Set the password (service principal secret) based on the Kerberos key of the storage account
+$kerbKey1 = Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName -ListKerbKey | Where-Object { $_.KeyName -like "kerb1" }
+$aadPasswordBuffer = [System.Linq.Enumerable]::Take([System.Convert]::FromBase64String($kerbKey1.Value), 32);
+$password = "kk:" + [System.Convert]::ToBase64String($aadPasswordBuffer);
+
+
+#Connect to Azure AD and retrieve the tenant information
+#When you encounter the error, please check to use Windows PowerShell. Don't use PowerShell core.
+Connect-AzureAD
+$azureAdTenantDetail = Get-AzureADTenantDetail;
+$azureAdTenantId = $azureAdTenantDetail.ObjectId
+$azureAdPrimaryDomain = ($azureAdTenantDetail.VerifiedDomains | Where-Object {$_._Default -eq $true}).Name
+
+#Generate the service principal names for the Azure AD service principal 
+$servicePrincipalNames = New-Object string[] 3
+$servicePrincipalNames[0] = 'HTTP/{0}.file.core.windows.net' -f $storageAccountName
+$servicePrincipalNames[1] = 'CIFS/{0}.file.core.windows.net' -f $storageAccountName
+$servicePrincipalNames[2] = 'HOST/{0}.file.core.windows.net' -f $storageAccountName
+
+#Create an application for the storage account
+$application = New-AzureADApplication -DisplayName $storageAccountName -IdentifierUris $servicePrincipalNames -GroupMembershipClaims "All";
+
+#Create a service principal for the storage account
+$servicePrincipal = New-AzureADServicePrincipal -AccountEnabled $true -AppId $application.AppId -ServicePrincipalType "Application";
+
+#Set the password for the storage account's service principal
+$Token = ([Microsoft.Open.Azure.AD.CommonLibrary.AzureSession]::AccessTokens['AccessToken']).AccessToken
+$Uri = ('https://graph.windows.net/{0}/{1}/{2}?api-version=1.6' -f $azureAdPrimaryDomain, 'servicePrincipals', $servicePrincipal.ObjectId)
+$json = @'
+{
+  "passwordCredentials": [
+  {
+    "customKeyIdentifier": null,
+    "endDate": "<STORAGEACCOUNTENDDATE>",
+    "value": "<STORAGEACCOUNTPASSWORD>",
+    "startDate": "<STORAGEACCOUNTSTARTDATE>"
+  }]
+}
+'@
+$now = [DateTime]::UtcNow
+$json = $json -replace "<STORAGEACCOUNTSTARTDATE>", $now.AddDays(-1).ToString("s")
+  $json = $json -replace "<STORAGEACCOUNTENDDATE>", $now.AddMonths(6).ToString("s")
+$json = $json -replace "<STORAGEACCOUNTPASSWORD>", $password
+$Headers = @{'authorization' = "Bearer $($Token)"}
+try {
+  Invoke-RestMethod -Uri $Uri -ContentType 'application/json' -Method Patch -Headers $Headers -Body $json 
+  Write-Host "Success: Password is set for $storageAccountName"
+} catch {
+  Write-Host $_.Exception.ToString()
+  Write-Host "StatusCode: " $_.Exception.Response.StatusCode.value
+  Write-Host "StatusDescription: " $_.Exception.Response.StatusDescription
+}
+
